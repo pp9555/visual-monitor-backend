45996488c62abd42f29e35a383143f37bc47e352
pp9555 <160492858+pp9555@users.noreply.github.com>
2026-05-12 13:45:32 +0530

    Fix hardcoded DB to use dotenv

--- worker.js
+++ worker.js
@@ -183,7 +183,7 @@ dotenv.config();
 puppeteer.use(StealthPlugin());
 
 // --- CONFIGURATION ---
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/visual-saas';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // Cloudinary Setup - REPLACE THESE WITH YOUR KEYS!
 cloudinary.config({ 
